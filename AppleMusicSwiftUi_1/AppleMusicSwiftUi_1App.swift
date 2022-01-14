7ca4cf4922c4f014ae62d681a6def778d8458ffd
Ani poletich <poletichanna@mail.ru>
2022-01-14 15:33:57 +0300

    Initial Commit

---
+++ AppleMusicSwiftUi_1/AppleMusicSwiftUi_1App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicSwiftUi_1App.swift
+//  AppleMusicSwiftUi_1
+//
+//  Created by anna on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicSwiftUi_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
