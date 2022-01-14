09d3d5e5101fff07c3896cb2010f361a752c0dd3
Ani poletich <poletichanna@mail.ru>
2022-01-14 16:54:47 +0300

    Create radio view

--- AppleMusicSwiftUi_1/RadioView/RadioView.swift
+++ AppleMusicSwiftUi_1/RadioView/RadioView.swift
@@ -5,4 +5,20 @@
 //  Created by anna on 14.01.2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct RadioView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+            }
+            .accentColor(.red)
+            .navigationTitle("Радио")
+        }
+    }
+    struct RadioView_Previews: PreviewProvider {
+        static var previews: some View {
+            RadioView()
+        }
+    }
+}
